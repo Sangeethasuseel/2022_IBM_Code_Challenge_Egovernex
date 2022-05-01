18d195db21b7ee5fcd96eebfd58844ce52637a86
Sangeethasuseel <58900533+Sangeethasuseel@users.noreply.github.com>
2022-05-01 21:12:40 +0530

    first commit

---
+++ lib/Components/textbox_button.dart
@@ -0,0 +1,153 @@
+import 'package:egovernex/Utils/constants.dart';
+import 'package:flutter/material.dart';
+
+class TextBox extends StatefulWidget {
+  final TextInputType textInputType;
+  final String hint;
+  final TextEditingController controller;
+  final String fieldName;
+
+  TextBox({required this.textInputType, required this.hint, required this.controller, required this.fieldName});
+
+  @override
+  _TextBoxState createState() => _TextBoxState();
+}
+
+class _TextBoxState extends State<TextBox> {
+  String? validate(String value, String type) {
+    if (value.isEmpty) {
+      return 'This field is required';
+    }
+
+    switch (type) {
+      case "year":
+        if (int.parse(value) < 2021 || int.parse(value) > 2031) {
+          return 'Please enter a valid year';
+        }
+        break;
+
+      case "password":
+        if (value.length < 6) {
+          return 'Password should be at least 6 characters';
+        }
+        break;
+
+      case "register":
+        if (value.length != 8) {
+          return 'Register number should be 8 digits long';
+        }
+        break;
+
+      case "phone":
+        if (value.length != 10) {
+          return 'Phone number should be 10 digits long';
+        }
+        break;
+
+      case "email":
+        Pattern pattern =
+            r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]"
+            r"{0,253}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]"
+            r"{0,253}[a-zA-Z0-9])?)*$";
+        RegExp regex = new RegExp(pattern.toString());
+        if (!regex.hasMatch(value) || value == null)
+          return 'Enter a valid email address';
+        break;
+    }
+
+    return null;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.fromLTRB(5.0, 5.0, 5.0, 5.0),
+      child: TextFormField(
+        obscureText: false,
+        autocorrect: false,
+        style: TextStyle(color: Colors.black87),
+        keyboardType: widget.textInputType,
+        validator: (value) {
+          String? result = validate(value!, widget.fieldName);
+          return result;
+        },
+        // autofocus: true,
+        controller: widget.controller,
+        decoration: InputDecoration(
+          isDense: true,
+          hintText: widget.hint,
+          hintStyle: TextStyle(color: Colors.white70),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(15.0),
+            borderSide: BorderSide(
+              color: Color(0xFF54ABD0),
+              width: 2,
+            ),
+          ),
+          contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+          disabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(15.0),
+            borderSide: BorderSide(
+              color: Color(0xFF54ABD0),
+              width: 2,
+            ),
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(15.0),
+            borderSide: BorderSide(
+              color: Color(0xFF54ABD0),
+              width: 2,
+            ),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(15.0),
+            borderSide: BorderSide(color: Color(0xFF54ABD0), width: 2),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Button extends StatefulWidget {
+  final String buttonName;
+  final Function onTap;
+
+  Button({required this.buttonName, required this.onTap});
+
+  @override
+  _ButtonState createState() => _ButtonState();
+}
+
+class _ButtonState extends State<Button> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(5),
+      child: Container(
+        color: Colors.transparent,
+        width: MediaQuery.of(context).size.width,
+        height: 50,
+        child: InkWell(
+          onTap: widget.onTap(),
+          child: Container(
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15.0),
+                gradient: LinearGradient(colors: [kBlue, kCyan])),
+            child: Center(
+              child: Text(
+                widget.buttonName,
+                style: TextStyle(
+                    color: Colors.black87,
+                    fontSize: 22.0,
+                    fontWeight: FontWeight.bold,
+                    shadows: [Shadow(offset: Offset(1, 1), blurRadius: 2)]
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/Pages/login.dart
@@ -0,0 +1,222 @@
+// import 'package:eduverse/Pages/main_page.dart';
+// import 'package:eduverse/Pages/onboarding.dart';
+// import 'package:eduverse/Services/database.dart';
+// import 'package:eduverse/Services/user.dart';
+import 'package:flutter/material.dart';
+import 'package:egovernex/Components/textbox_button.dart';
+// import 'package:rflutter_alert/rflutter_alert.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:shared_preferences/shared_preferences.dart';
+// import 'package:cloud_firestore/cloud_firestore.dart';
+
+// final FirebaseAuth _auth = FirebaseAuth.instance;
+// User firebaseUser = FirebaseAuth.instance.currentUser;
+
+class Login extends StatefulWidget {
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  // var alertStyle = AlertStyle(
+  //   overlayColor: Colors.transparent,
+  //   animationType: AnimationType.fromTop,
+  //   isCloseButton: false,
+  //   isOverlayTapDismiss: false,
+  //   descStyle: TextStyle(fontWeight: FontWeight.bold),
+  //   animationDuration: Duration(milliseconds: 400),
+  //   alertBorder: RoundedRectangleBorder(
+  //     borderRadius: BorderRadius.circular(60.0),
+  //     side: BorderSide(
+  //       color: Colors.grey,
+  //     ),
+  //   ),
+  //   titleStyle: TextStyle(
+  //     color: Color(0xFF54ABD0),
+  //   ),
+  // );
+
+  // Future saveUserInfo() async {
+  //   await DatabaseMethods()
+  //       .getUserRole(_auth.currentUser.uid)
+  //       .then((userRoleSnapshot) async {
+  //     await DatabaseMethods()
+  //         .getUserInfo(_auth.currentUser.uid, userRoleSnapshot.data()["role"])
+  //         .then((userInfoSnapshot) {
+  //       UserHelper.saveRole(userInfoSnapshot.data()["role"]);
+  //       UserHelper.saveName(userInfoSnapshot.data()["first_name"] +
+  //           " " +
+  //           userInfoSnapshot.data()["last_name"]);
+  //       UserHelper.saveBranch(
+  //           userInfoSnapshot.data()["branch"].toString().toLowerCase());
+  //     });
+  //   });
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Scaffold(
+        body: SafeArea(
+          child: Stack(
+            children: [
+              Container(
+
+                  child: Image.asset(
+                    'assets/images/banner_governex.jpg',
+                    width: MediaQuery.of(context).size.width,
+                    fit: BoxFit.fill,
+                  )),
+              ListView(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        // Container(
+                        //   child: Padding(
+                        //     padding: const EdgeInsets.all(10.0),
+                        //     child: Align(
+                        //       alignment: Alignment.topLeft,
+                        //       child: Text(
+                        //         "Welcome Back!",
+                        //         style: TextStyle(
+                        //           color: Colors.white,
+                        //           fontWeight: FontWeight.bold,
+                        //           fontSize: 26.0,
+                        //         ),
+                        //       ),
+                        //     ),
+                        //   ),
+                        // ),
+                        SizedBox(
+                          height: 200.0,
+                        ),
+                        Text('EGOVERNEX',
+                            style: TextStyle(
+                                fontWeight: FontWeight.w500,
+                                color: Colors.black87,
+                                fontSize: 22,
+                                letterSpacing: 2)),
+                        SizedBox(
+                          height: 80.0,
+                        ),
+                        TextBox(
+                          textInputType: TextInputType.emailAddress,
+                          hint: "Official Email",
+                          controller: _emailController, fieldName: '',
+                        ),
+                        TextBox(
+                          textInputType: TextInputType.visiblePassword,
+                          hint: "Password",
+                          controller: _passwordController, fieldName: '',
+                        ),
+                        Button(
+                            buttonName: "Sign In",
+                            onTap: ()  {
+                              // try {
+                              //   if (_formKey.currentState.validate()) {
+                              //     User user = (await FirebaseAuth.instance
+                              //         .signInWithEmailAndPassword(
+                              //       email: _emailController.text,
+                              //       password: _passwordController.text,
+                              //     ))
+                              //         .user;
+                              //     if (user != null) {
+                              //       await saveUserInfo();
+                              //       Navigator.of(context).pushReplacement(
+                              //           new MaterialPageRoute(
+                              //               builder: (context) => HomePage()));
+                              //       // Navigator.push(
+                              //       //     context,
+                              //       //     MaterialPageRoute(
+                              //       //       builder: (BuildContext context) => HomePage(),
+                              //       //     ));
+                              //     }
+                              //   }
+                              // } catch (e) {
+                              //   print(e);
+                              //   Alert(
+                              //     context: context,
+                              //     style: alertStyle,
+                              //     type: AlertType.info,
+                              //     title: "Invalid",
+                              //     desc: "Invalid username or password",
+                              //     buttons: [
+                              //       DialogButton(
+                              //         child: Text(
+                              //           "Close",
+                              //           style: TextStyle(
+                              //               color: Colors.white, fontSize: 20),
+                              //         ),
+                              //         onPressed: () => Navigator.pop(context),
+                              //         color: Color(0xFF54ABD0),
+                              //         radius: BorderRadius.circular(10.0),
+                              //       ),
+                              //     ],
+                              //   ).show();
+                              //   _emailController.text = "";
+                              //   _passwordController.text = "";
+                              //   // TODO: AlertDialog with error
+                              // }
+                            }),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 30.0, horizontal: 10.0),
+                          child: Center(
+                            child: Container(
+                                child: Row(
+                                  children: <Widget>[
+                                    Text(
+                                      "Don't have an account?",
+                                      textAlign: TextAlign.center,
+                                      style: TextStyle(
+                                        color: Colors.black87,
+                                        fontWeight: FontWeight.w700,
+                                        fontSize: 18.0,
+                                      ),
+                                    ),
+                                    FlatButton(
+                                      textColor: Color(0xFF55ACD1),
+                                      child: Text(
+                                        'Sign Up',
+                                        style: TextStyle(fontSize: 20),
+                                      ),
+                                      onPressed: () {}
+                                      // async {
+                                      //   //signup screen
+                                      //   var prefs =
+                                      //   await SharedPreferences.getInstance();
+                                      //   var boolKey = 'isFirstTime';
+                                      //   var isFirstTime =
+                                      //       prefs.getBool(boolKey) ?? true;
+                                      //   Navigator.pushReplacement(
+                                      //       context,
+                                      //       MaterialPageRoute(
+                                      //         builder: (BuildContext context) =>
+                                      //             LaunchScreen(prefs, boolKey),
+                                      //       ));
+                                      // },
+                                    )
+                                  ],
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                )),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
